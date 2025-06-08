f2a073785cf9a368cac2d027c642d45e55e29b8c
Alex Kharichev <myx@myx.nz>
2025-06-08 21:54:57 +0300

    + markdown support

--- sh-scripts/DistroLocalTools.sh
+++ sh-scripts/DistroLocalTools.sh
@@ -147,8 +147,9 @@ DistroLocalTools(){
 			set +e ; return 1
 		;;
 		--help-install-unix-bare)
+			# cat "$MMDAPP/.local/myx/myx.distro-.local/sh-lib/HelpDistroLocalTools-install-unix-bare.text" >&2
 			. "$MMDAPP/.local/myx/myx.distro-.local/sh-lib/DistroLocalCatMarkdown.include"
-			DistroLocalCatMarkdown "$MMDAPP/.local/myx/myx.distro-.local/sh-lib/HelpDistroLocalTools-unix-bare.md" >&2
+			DistroLocalCatMarkdown "$MMDAPP/.local/myx/myx.distro-.local/sh-lib/HelpDistroLocalTools-install-unix-bare.md" >&2
 			exit 1;
 		;;
 		*)
