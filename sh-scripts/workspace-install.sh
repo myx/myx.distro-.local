6da8987855e52b273449e318b8c12e29a9317673
Alex Kharichev <myx@myx.nz>
2025-07-04 16:00:41 +0300

    * minor: -x workspace

--- sh-scripts/workspace-install.sh
+++ sh-scripts/workspace-install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-set -x
 ###############################################################################
 #
 # This is 'boot' standalone installer script, that allows to bootstrap and install
